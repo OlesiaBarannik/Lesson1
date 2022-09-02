df325d84bca13986f256a1d8ac7e05625cee8184
OlesiaBarannik <112662408+OlesiaBarannik@users.noreply.github.com>
2022-09-02 11:22:28 +0300

    Added tasks #2,3 Lesson1

---
+++ Task2.py
@@ -0,0 +1,7 @@
+x = "Hello Beetroot"
+# Hello Beetroot, Hello Beetroot, Hello Beetroot
+print(x, x, x, sep=", ", end="\n\n")
+s = "Добрий день, Everybody"
+# Добрий день, Everybody!
+# Добрий день, Everybody!
+print(s, s, sep="!\n", end="!\n")
---
+++ Task3.py
@@ -0,0 +1,15 @@
+q = "#"
+print(q*9, sep="\n")
+
+for number in range(0,3) :
+    print(q, q, sep="\t\t", end="\n")
+
+print(q*9, sep="\n", end="\n\n")
+
+for number in range(0,2) :
+    print(q, q, sep="\t\t", end="\n")
+
+print(q*9, sep="\n")
+
+for number in range(0,2) :
+    print(q, q, sep="\t\t", end="\n")
